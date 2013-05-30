2a8e7b3bad393a5003c3f56e574676eb6182a611
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2013-05-30 18:13:36 -0400

    Add test suite runs to Gershwin build, keep separate from Clojure compile/test

---
+++ test/gershwin/test_gershwin/core.clj
@@ -0,0 +1,6 @@
+(ns gershwin.test-gershwin.core
+  (:require-words [gershwin.test :refer [unit-test]]))
+
+: suite [-- ?]
+  ;; Invocation/Application
+  #[ 42 ] #[ {:answer 42} :answer apply ] unit-test .
