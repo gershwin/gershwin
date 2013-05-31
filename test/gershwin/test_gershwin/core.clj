999bfefca2eb734cff3126f8ced85c1957c18afc
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2013-05-30 23:13:03 -0400

    Start porting basic apply tests from prototype impl.

--- test/gershwin/test_gershwin/core.clj
+++ test/gershwin/test_gershwin/core.clj
@@ -3,4 +3,7 @@
 
 : suite [-- ?]
   ;; Invocation/Application
-  #[ 42 ] #[ {:answer 42} :answer apply ] unit-test .
+  #[ 42 ] #[ {:answer 42} :answer apply ] unit-test
+  #[ 42 ] #[ 21 21 clojure.core/+ apply2 ] unit-test
+  #[ "bazbarfoo" ] #[ "foo" "bar" "baz" clojure.core/str apply3 ] unit-test
+  #[ 1 ] #[ 3 2 clojure.core/- apply-swap ] unit-test .
