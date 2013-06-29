513249d66ccca493316540faed148bc7f04acaf1
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2013-06-29 00:22:58 -0400

    Add shell script to start nREPL with Maven-installed artifacts

--- src/script/nrepl.sh
+++ src/script/nrepl.sh
@@ -1,5 +1,19 @@
-(require '[clojure.tools.nrepl.server :refer [start-server]])
-(require '[clojure.pprint :refer [pprint]])
-(defonce server (start-server))
-(println "Started nREPL server with following specifications:")
-(pprint (into {} server))
+#!/bin/bash
+
+#
+# Usage: nrepl.sh <port-number>
+#
+
+EXPECTED_ARGS=1
+E_BADARGS=65
+
+DO_showUsage() {
+    echo "Usage: $(basename $0) <port-number>"
+    exit $E_BADARGS
+}
+
+if [ $# -ne $EXPECTED_ARGS ]; then
+    DO_showUsage
+fi
+
+java -cp $HOME/.m2/repository/org/gershwinlang/gershwin/0.2.0-master-SNAPSHOT/gershwin-0.2.0-master-SNAPSHOT.jar:$HOME/.m2/repository/org/gershwinlang/tools.nrepl/0.2.0-master-SNAPSHOT/tools.nrepl-0.2.0-master-SNAPSHOT.jar gershwin.main -e "(do (require '[clojure.tools.nrepl.server :refer [start-server]]) (start-server :port $1))"
